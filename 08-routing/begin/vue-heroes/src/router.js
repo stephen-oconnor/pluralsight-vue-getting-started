978ddbc82731dd50f1cb8aca36763e004a00ac24
Stephen O'Connor <stephen.oconnor@collectivworks.com>
2020-05-19 17:28:29 +0100

    08 navigating vue apps with routing complete.

---
+++ 08-routing/begin/vue-heroes/src/router.js
@@ -0,0 +1,42 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import PageNotFound from './views/page-not-found';
+
+Vue.use(Router);
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      redirect: '/heroes',
+    },
+    {
+      path: '/heroes',
+      name: 'heroes',
+      component: () =>
+        import(/* webpackChunkName: "bundle-heroes" */ './views/heroes.vue'),
+    },
+    {
+      path: '/heroes/:id',
+      name: 'hero-detail',
+      component: () =>
+        import(/* webpackChunkName: "bundle-heroes" */ './views/hero-detail.vue'),
+      props: route => ({ id: parseInt(route.params.id) }),
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+        import(/* webpackChunkName: "bundle-about" */ './views/about.vue'),
+    },
+    {
+      path: '*',
+      component: PageNotFound,
+    },
+  ],
+});
